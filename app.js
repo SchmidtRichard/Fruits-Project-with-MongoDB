5fd83abbd2c0954f920fc8e8f08f46584bbf4abb
SchmidtRichard <ri.schmidta@gmail.com>
2021-03-15 11:53:48 +0000

    Reading from DB with Mongoose

--- app.js
+++ app.js
@@ -1,83 +1,115 @@
-//Connect to MongoDB
-const MongoClient = require('mongodb').MongoClient;
+//Require mongoose
+const mongoose = require('mongoose');
 
-//Assert has to do with testing, validates data entry and connection to MongoDB
-const assert = require('assert');
-
-//Connection URL
-const url = 'mongodb://localhost:27017';
-
-//Database Name
-const dbName = 'fruitsDB';
-
-//Create a new MongoClient
-const client = new MongoClient(url, {
+//Connect to the mongoDB server and then look for a DB called fruitsDB and connect to it, if it does not exist it will create it instead
+mongoose.connect("mongodb://localhost:27017/fruitsDB", {
+  useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
-//Use connect method to connect to the server
-client.connect(function(err) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-
-  const db = client.db(dbName);
-
-  //Insert documents to the fruitsDB
-  //insertDocuments(db, function() {
-
-  //find documents in the fruitsDB
-  findDocuments(db, function() {
-    client.close();
-  });
+//Create a new schema (blueprint, structure of the data that will be saved into the mongoDB database)
+const fruitSchema = new mongoose.Schema({
+  name: String,
+  rating: Number,
+  review: String
 });
 
-//Create a new collection (fruits)
-const insertDocuments = function(db, callback) {
+//Use the schema to create a mongoose model - 1st parameter is the name of the collection that will comply with the fruitSchema schema, 2nd parameter is the schema (fruitSchema)
+const Fruit = mongoose.model("Fruit", fruitSchema);
 
-  //Get the documents collection
-  const collection = db.collection('fruits');
+//Create a new fruit document
+const fruit = new Fruit({
+  name: "Apple",
+  rating: 7,
+  review: "Pretty good"
+});
 
-  //Insert some documents
-  collection.insertMany([{
-      name: "Apple",
-      score: 8,
-      review: "Great fruit"
-    },
-    {
-      name: "Orange",
-      score: 6,
-      review: "Kinda sour"
-    },
-    {
-      name: "Banana",
-      score: 9,
-      review: "Great stuff"
-    }
-  ], function(err, result) {
-    //Validate to make sure there are no errors
-    assert.equal(err, null);
+//Calls the save method in mongoose to save the fruit document above (Apple) into a Fruits collection inside the fruitsDB
+fruit.save();
 
-    //Make sure there are 3 results inserted into the collection
-    assert.equal(3, result.result.n);
-    assert.equal(3, result.ops.length);
+//Create a new schema for a person
+const personSchema = new mongoose.Schema({
+  name: String,
+  age: Number
+});
 
-    //If it passes the above aserts we console.log the below
-    console.log("Inserted 3 documents into the collection");
-    callback(result);
-  });
-};
+//Create a mongoose model schema for a person
+const Person = mongoose.model("Person", personSchema);
 
-//Find all documents
-const findDocuments = function(db, callback) {
+//Create a new person to the doc
+const person = new Person({
+  name: "John",
+  age: 30
+});
 
-  //Get the documents collection
-  const collection = db.collection('fruits');
+person.save();
 
-  //Find some documents and add them to an array
-  collection.find({}).toArray(function(err, fruits) {
-    assert.equal(err, null);
-    console.log("Found the following records");
+
+
+const kiwi = new Fruit({
+  name: "Kiwi",
+  score: 10,
+  review: "Great fruit"
+});
+
+
+const orange = new Fruit({
+  name: "Orange",
+  score: 8,
+  review: "Sweet"
+});
+
+const banana = new Fruit({
+  name: "Banana",
+  score: 10,
+  review: "Great fruit"
+});
+
+
+//Specify the name of the mangoose model (Fruit), which will allow it to connect to the relevant collection, and also know the schema it should work with
+//It takes 2 parameters, the 1st one is an array of objects that match the schema, the second one is a callback which allow us to log any errors if there were any issues inserting all the objects into the fruits collection
+Fruit.insertMany([kiwi, orange, banana], function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Succesfully saved all the fruits to the fruitsDB")
+  }
+});
+
+
+//Reading from DB with Mongoose
+Fruit.find(function(err, fruits) {
+  if (err) {
+    console.log(err);
+  } else {
+
+    //Close the connection after performing the find of all the fruits
+    mongoose.connection.close();
+
+    //Console.log all the fruit array
     console.log(fruits);
-    callback(fruits);
-  });
-}
+
+    //Loop through the array fruits
+    fruits.forEach(function(fruit) {
+      //Inside the callback console.log each fruit using the name property
+      console.log(fruit.name);
+    });
+  }
+});
+
+
+// //Find all documents
+// const findDocuments = function(db, callback) {
+//
+//   //Get the documents collection
+//   const collection = db.collection('fruits');
+//
+//   //Find some documents and add them to an array
+//   collection.find({}).toArray(function(err, fruits) {
+//     assert.equal(err, null);
+//     console.log("Found the following records");
+//     console.log(fruits);
+//     callback(fruits);
+//
+//   });
+// }
